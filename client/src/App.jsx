bb1320e5e89a1afc06a4a5dbf4c450f13f08f9a2
Bo01la <abdoehab30@gmail.com>
2025-08-26 03:16:15 +0300

    nav bar done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
+import gsap from "gsap";
+// imposting gsap plugins
+import { ScrollTrigger, SplitText } from "gsap/all";
 
+// registering the plugins
+// registering them in the App makes them registered globally
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+import Nav from "./components/Nav";
 
 function App() {
-
-
   return (
-    <>
-      <h1 className="text-9xl text-amber-900 font-black" >hello world</h1>
-    </>
-  )
+    <main>
+      <Nav />
+    </main>
+  );
 }
 
-export default App
+export default App;
