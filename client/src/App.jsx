04d6736b99a8536fe9a705d8169854a8ec363f42
Bo01la <abdoehab30@gmail.com>
2025-08-29 22:54:18 +0300

    done with contact section and menu section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,8 @@ import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails.jsx";
 import About from "./components/About.jsx";
 import Art from "./components/Art.jsx";
+import Menu from "./components/Menu.jsx";
+import Contact from "./components/Contact.jsx";
 
 function App() {
   return (
@@ -20,6 +22,8 @@ function App() {
       <Cocktails />
       <About />
       <Art />
+      <Menu />
+      <Contact />
     </main>
   );
 }
